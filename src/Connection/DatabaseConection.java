1b6c01611aea0aef57733bca49b22537add2ca5e
Dorin V <dorineltuta2006@yahoo.com>
2016-05-28 01:37:18 +0300

    Database Connection

--- src/Connection/DatabaseConection.java
+++ src/Connection/DatabaseConection.java
@@ -13,63 +13,8 @@ public class DatabaseConection {
 	private static final String DB_PASSWORD = "parola";
 	
 	
-	public static void main(String[] argv) {
-
-		try {
-
-			insertRecordIntoTable();
-
-		} catch (SQLException e) {
-
-			System.out.println(e.getMessage());
-
-		}
-
-	}
-
-	private static void insertRecordIntoTable() throws SQLException {
-
-		Connection dbConnection = null;
-		PreparedStatement preparedStatement = null;
-
-		String insertTableSQL = "INSERT INTO login"
-				+ "(ID, USERNAME, PASSWORD, MOD) VALUES"
-				+ "(?,?,?,?)";
-
-		try {
-			dbConnection = getDBConnection();
-			preparedStatement = dbConnection.prepareStatement(insertTableSQL);
-
-			preparedStatement.setInt(1, 11);
-			preparedStatement.setString(2, "Medic");
-			preparedStatement.setString(3, "pass");
-			preparedStatement.setString(4, "Medic");
-			//preparedStatement.setTimestamp(4, getCurrentTimeStamp());
-
-			// execute insert SQL stetement
-			preparedStatement.executeUpdate();
-
-			System.out.println("Record is inserted into DBUSER table!");
-
-		} catch (SQLException e) {
-
-			System.out.println(e.getMessage());
-
-		} finally {
-
-			if (preparedStatement != null) {
-				preparedStatement.close();
-			}
-
-			if (dbConnection != null) {
-				dbConnection.close();
-			}
-
-		}
-
-	}
-
-	private static Connection getDBConnection() {
+	
+	public static Connection getDBConnection() {
 
 		Connection dbConnection = null;
 
@@ -95,12 +40,4 @@ public class DatabaseConection {
 		return dbConnection;
 
 	}
-
-	private static java.sql.Timestamp getCurrentTimeStamp() {
-
-		java.util.Date today = new java.util.Date();
-		return new java.sql.Timestamp(today.getTime());
-
-	}
-
 }
