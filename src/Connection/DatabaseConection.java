713e54e06f5089d117695ebba50a837493787795
Dorin V <dorineltuta2006@yahoo.com>
2016-05-28 01:46:01 +0300

    Database Connection Added

--- src/Connection/DatabaseConection.java
+++ src/Connection/DatabaseConection.java
@@ -13,68 +13,13 @@ public class DatabaseConection {
 	private static final String DB_PASSWORD = "parola";
 	
 	
-	public static void main(String[] argv) {
-
-		try {
-
-			insertRecordIntoTable();
-
-		} catch (SQLException e) {
-
-			System.out.println(e.getMessage());
-
-		}
-
-	}
-
-	private static void insertRecordIntoTable() throws SQLException {
-
-		Connection dbConnection = null;
-		PreparedStatement preparedStatement = null;
-
-		String insertTableSQL = "INSERT INTO login"
-				+ "(ID, USERNAME, PASSWORD, MOD) VALUES"
-				+ "(?,?,?,?)";
-
-		try {
-			dbConnection = getDBConnection();
-			preparedStatement = dbConnection.prepareStatement(insertTableSQL);
-
-			preparedStatement.setInt(1, 11);
-			preparedStatement.setString(2, "Medic");
-			preparedStatement.setString(3, "pass");
-			preparedStatement.setString(4, "Medic");
-			//preparedStatement.setTimestamp(4, getCurrentTimeStamp());
-
-			// execute insert SQL stetement
-			preparedStatement.executeUpdate();
-
-			System.out.println("Record is inserted into DBUSER table!");
-
-		} catch (SQLException e) {
-
-			System.out.println(e.getMessage());
-
-		} finally {
-
-			if (preparedStatement != null) {
-				preparedStatement.close();
-			}
-
-			if (dbConnection != null) {
-				dbConnection.close();
-			}
-
-		}
-
-	}
-
-	private static Connection getDBConnection() {
+	
+	public static Connection getDBConnection() {
 
 		Connection dbConnection = null;
 
 		try {
-
+			// This will load the MySQL driver, each DB has its own driver
 			Class.forName(DB_DRIVER);
 
 		} catch (ClassNotFoundException e) {
@@ -84,7 +29,7 @@ public class DatabaseConection {
 		}
 
 		try {
-
+			// Setup the connection with the DB
 			dbConnection = DriverManager.getConnection(DB_CONNECTION, DB_USER,DB_PASSWORD);
 			return dbConnection;
 
@@ -95,12 +40,4 @@ public class DatabaseConection {
 		return dbConnection;
 
 	}
-
-	private static java.sql.Timestamp getCurrentTimeStamp() {
-
-		java.util.Date today = new java.util.Date();
-		return new java.sql.Timestamp(today.getTime());
-
-	}
-
 }
